0967a299569bf944fa377344933a816126edef94
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-08-20 10:47:28 -0700

    started working on forgot password page

--- config/urls.js
+++ config/urls.js
@@ -2,3 +2,4 @@ export const baseUrl = "https://evov.onrender.com";
 
 export const register = `${baseUrl}/api/v1/register`;
 export const login = `${baseUrl}/api/v1/login`;
+export const resetPassword = `${baseUrl}/api/v1/reset-password`;
---
+++ js/reset.js
@@ -0,0 +1,40 @@
+import { resetPassword } from "../config/urls.js";
+// Global
+const form = document.querySelector("form");
+const err = document.querySelector(".error");
+const buttonText = document.querySelector(".button-text");
+const spinner = document.querySelector(".loading");
+const modal = document.querySelector(".modal-container");
+
+// Handle button loading state
+const toggleLoadingState = () => {
+  buttonText.classList.toggle("hide-button-text");
+  spinner.classList.toggle("show-spinner");
+};
+
+// Handle Modal
+const showModal = () => {
+  modal.classList.toggle("show-modal");
+};
+
+// Handle form submission
+const handleFormSubmit = async (e) => {
+  e.preventDefault();
+  toggleLoadingState();
+  const formData = {
+    newPassword: document.querySelector("#newPassword").value,
+    confirmPassword: document.querySelector("#confirmPassword").value,
+  };
+  try {
+    const res = await axios.post(resetPassword, formData);
+    toggleLoadingState();
+  } catch (error) {
+    toggleLoadingState();
+    err.innerHTML = error.response.data.msg;
+  }
+};
+
+form.addEventListener("submit", handleFormSubmit);
+form.addEventListener("focusin", () => {
+  err.innerHTML = "";
+});
